1be490314bc4818bc7000bcc3b9fb37f561da8d8
jeroz1 <44138303+jeroz1@users.noreply.github.com>
2019-02-02 16:38:25 -0500

    Adding Ravencoin Settings

--- start.sh
+++ start.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-# --- bitcoin mainnet: f9beb4d9 (db = 0) ---
-python -u crawl.py conf/crawl.f9beb4d9.conf master > log/crawl.f9beb4d9.master.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.1.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.2.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.3.out 2>&1 &
+# --- Ravencoin mainnet: 5241564e (db = 0) ---
+python2 -u crawl.py conf/crawl.conf.default master > log/crawl.5241564e.master.out 2>&1 &
+python2 -u crawl.py conf/crawl.conf.default slave > log/crawl.5241564e.slave.1.out 2>&1 &
+python2 -u crawl.py conf/crawl.conf.default slave > log/crawl.5241564e.slave.2.out 2>&1 &
+python2 -u crawl.py conf/crawl.conf.default slave > log/crawl.5241564e.slave.3.out 2>&1 &
+python2 -u crawl.py conf/crawl.conf.default slave > log/crawl.5241564e.slave.4.out 2>&1 &
+python2 -u crawl.py conf/crawl.conf.default slave > log/crawl.5241564e.slave.5.out 2>&1 &
 
-python -u ping.py conf/ping.f9beb4d9.conf master > log/ping.f9beb4d9.master.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.1.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.2.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.3.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.4.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.5.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.6.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default master > log/ping.5241564e.master.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.1.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.2.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.3.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.4.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.5.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.6.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.7.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.8.out 2>&1 &
+python2 -u ping.py conf/ping.conf.default slave > log/ping.5241564e.slave.9.out 2>&1 &
 
-python -u resolve.py conf/resolve.f9beb4d9.conf > log/resolve.f9beb4d9.out 2>&1 &
+python2 -u resolve.py conf/resolve.conf.default > log/resolve.5241564e.out 2>&1 &
 
-python -u export.py conf/export.f9beb4d9.conf > log/export.f9beb4d9.out 2>&1 &
+python2 -u export.py conf/export.conf.default > log/export.5241564e.out 2>&1 &
 
-python -u seeder.py conf/seeder.f9beb4d9.conf > log/seeder.f9beb4d9.out 2>&1 &
+python2 -u seeder.py conf/seeder.conf.default > log/seeder.5241564e.out 2>&1 &
 
-python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.1.out 2>&1 &
-python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.2.out 2>&1 &
+python2 -u pcap.py conf/pcap.conf.default > log/pcap.5241564e.1.out 2>&1 &
+python2 -u pcap.py conf/pcap.conf.default > log/pcap.5241564e.2.out 2>&1 &
+python2 -u pcap.py conf/pcap.conf.default > log/pcap.5241564e.3.out 2>&1 &
